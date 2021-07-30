c3a9836482b3d74bc691277cf268a378a6c10f6e
Anton Avramov <lukav@lukav.com>
2021-07-30 02:45:47 -0400

    Fix headers propagation Added HEAD method

--- simpleProxy.py
+++ simpleProxy.py
@@ -2,17 +2,49 @@
 import socketserver
 import http.server
 import urllib.request
-PORT = 8089
+import urllib.error
+PORT = 8088
+
+class NoRedirect(urllib.request.HTTPRedirectHandler):
+    def redirect_request(self, req, fp, code, msg, headers, newurl):
+        return None
+
+
+opener = urllib.request.build_opener(NoRedirect)
+urllib.request.install_opener(opener)
 
 class Proxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         url=self.path.lstrip('/?')
-        self.send_response(200)
-        self.end_headers()
         headers=self.headers
         del headers['Host']
         req = urllib.request.Request(url, headers=headers)
-        self.copyfile(urllib.request.urlopen(req), self.wfile)
+        try:
+            res = urllib.request.urlopen(req)
+        except urllib.error.HTTPError as e:
+            res = e
+        self.log_request(res.status)
+        self.send_response_only(res.status)
+        for key, val in res.getheaders():
+            self.send_header(key, val)
+        self.end_headers()
+        self.copyfile(res, self.wfile)
+
+    def do_HEAD(self):
+        url=self.path.lstrip('/?')
+        headers=self.headers
+        del headers['Host']
+        req = urllib.request.Request(url, headers=headers, method='HEAD')
+        try:
+            res = urllib.request.urlopen(req)
+        except urllib.error.HTTPError as e:
+            res = e
+        self.log_request(res.status)
+        self.send_response_only(res.status)
+        for key, val in res.getheaders():
+            self.send_header(key, val)
+        self.end_headers()
+        self.copyfile(res, self.wfile)
 
 httpd = socketserver.ForkingTCPServer(('', PORT), Proxy)
 print ("Now serving at", str(PORT))
