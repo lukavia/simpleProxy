fbb710a34a5c4fa394b199d802b5078f5003bd47
Anton Avramov <lukav@lukav.com>
2021-07-30 20:51:36 -0400

    Polish comments and style

--- simpleProxy.py
+++ simpleProxy.py
@@ -5,46 +5,43 @@ import urllib.request
 import urllib.error
 PORT = 8088
 
+# Do not follow redirect just return whatever the other server returns
 class NoRedirect(urllib.request.HTTPRedirectHandler):
     def redirect_request(self, req, fp, code, msg, headers, newurl):
         return None
-
 
 opener = urllib.request.build_opener(NoRedirect)
 urllib.request.install_opener(opener)
 
 class Proxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        url=self.path.lstrip('/?')
-        headers=self.headers
-        del headers['Host']
-        req = urllib.request.Request(url, headers=headers)
-        try:
-            res = urllib.request.urlopen(req)
-        except urllib.error.HTTPError as e:
-            res = e
-        self.log_request(res.status)
-        self.send_response_only(res.status)
-        for key, val in res.getheaders():
-            self.send_header(key, val)
-        self.end_headers()
+        res = self.do_request(method='GET')
         self.copyfile(res, self.wfile)
 
     def do_HEAD(self):
+        self.do_request(method='HEAD')
+
+    def do_request(self, method='GET'):
+        # remove the separator so it only leaves http....
         url=self.path.lstrip('/?')
+        # get the headers and delete the proxy Host
         headers=self.headers
         del headers['Host']
-        req = urllib.request.Request(url, headers=headers, method='HEAD')
+        # Make the request to the server
+        req = urllib.request.Request(url, headers=headers, method=method)
         try:
             res = urllib.request.urlopen(req)
         except urllib.error.HTTPError as e:
             res = e
+        # Log the request and return the status and headers
         self.log_request(res.status)
         self.send_response_only(res.status)
         for key, val in res.getheaders():
             self.send_header(key, val)
         self.end_headers()
-        self.copyfile(res, self.wfile)
+
+        # return the result for further processing
+        return res
 
 httpd = socketserver.ForkingTCPServer(('', PORT), Proxy)
 print ("Now serving at", str(PORT))
